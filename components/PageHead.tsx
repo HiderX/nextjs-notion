d03a3690fb857d8ecd8320aa87d7ebfcdb4c5cf3
hiderx <3193997905@qq.com>
2025-12-06 16:38:40 +0800

    chore: update favicon and clean up old icon files

--- components/PageHead.tsx
+++ components/PageHead.tsx
@@ -98,6 +98,13 @@ export function PageHead({
         title={site?.name}
       />
 
+      {/* Favicon and app icons */}
+      <link rel='icon' type='image/x-icon' href='/favicon.ico' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
+      <link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+      <link rel='manifest' href='/site.webmanifest' />
+
       <meta property='og:title' content={title} />
       <meta name='twitter:title' content={title} />
       <title>{title}</title>
